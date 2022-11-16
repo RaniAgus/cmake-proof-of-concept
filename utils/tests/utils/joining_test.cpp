c57fefb5be805372154ae5d8ff61b061c9329cf9
Agustin Ranieri <aguseranieri@gmail.com>
2022-11-16 11:08:27 -0300

    add test example using doctest

---
+++ utils/tests/utils/joining_test.cpp
@@ -0,0 +1,41 @@
+#include <doctest/doctest.h>
+#include <string>
+
+extern "C" {
+#include "utils/joining.h"
+}
+
+using std::string;
+
+TEST_SUITE("Joining") {
+    t_list *ayudantes;
+    char *result;
+
+    TEST_CASE("List of strings") {
+        ayudantes = list_create();
+
+        SUBCASE("Should join an empty list") {
+            result = join_string_list(ayudantes);
+            REQUIRE_EQ(string(result), "[]");
+        }
+
+        list_add(ayudantes, string_duplicate("Agustin"));
+
+        SUBCASE("Should join a list with one element") {
+            result = join_string_list(ayudantes);
+            REQUIRE_EQ(string(result), "[Agustin]");
+        }
+
+        list_add(ayudantes, string_duplicate("Damian"));
+        list_add(ayudantes, string_duplicate("Dario"));
+        list_add(ayudantes, string_duplicate("Juan"));
+
+        SUBCASE("Should join a list with multiple elements") {
+            result = join_string_list(ayudantes);
+            REQUIRE_EQ(string(result), "[Agustin, Damian, Dario, Juan]");
+        }
+
+        list_destroy_and_destroy_elements(ayudantes, free);
+        free(result);
+    }
+}
