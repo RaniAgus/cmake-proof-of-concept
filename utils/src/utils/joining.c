bd802727ea6f01e17b56f361a5739e80eb3bc038
Agustin Ranieri <aguseranieri@gmail.com>
2022-11-16 12:21:32 -0300

    update example

--- utils/src/utils/joining.c
+++ utils/src/utils/joining.c
@@ -1,16 +1,21 @@
 #include "joining.h"
 
-char *merge_strings(char *accumulated, char *element) {
-    if (string_equals_ignore_case(accumulated, "[")) {
-        string_append(&accumulated, element);
-    } else {
-        string_append_with_format(&accumulated, ", %s", element);
-    }
-    return accumulated;
-}
+char *merge_strings(char *accumulated, char *element);
 
 char *join_string_list(t_list *list) {
-    char *result = list_fold(list, string_duplicate("["), (void *) merge_strings);
+    if (list_is_empty(list)) {
+        return string_duplicate("[]");
+    }
+
+    char *result = list_fold(list, string_new(), (void *) merge_strings);
     string_append(&result, "]");
     return result;
+}
+
+// Private functions
+
+char *merge_strings(char *accumulated, char *element) {
+    char* prefix = string_is_empty(accumulated) ? "[" : ", ";
+    string_append_with_format(&accumulated, "%s%s", prefix, element);
+    return accumulated;
 }
