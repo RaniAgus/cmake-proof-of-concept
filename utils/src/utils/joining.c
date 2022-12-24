af9de843974306c221dee19a61c027293681a791
Agustin Ranieri <aguseranieri@gmail.com>
2022-12-24 19:41:14 -0300

    add nested functions usage

--- utils/src/utils/joining.c
+++ utils/src/utils/joining.c
@@ -1,21 +1,18 @@
 #include "joining.h"
 
-char *merge_strings(char *accumulated, char *element);
-
 char *join_string_list(t_list *list) {
     if (list_is_empty(list)) {
         return string_duplicate("[]");
     }
 
+    char *merge_strings(char *accumulated, char *element) {
+        char* prefix = string_is_empty(accumulated) ? "[" : ", ";
+        string_append_with_format(&accumulated, "%s%s", prefix, element);
+        return accumulated;
+    }
+
     char *result = list_fold(list, string_new(), (void *) merge_strings);
     string_append(&result, "]");
+
     return result;
-}
-
-// Private functions
-
-char *merge_strings(char *accumulated, char *element) {
-    char* prefix = string_is_empty(accumulated) ? "[" : ", ";
-    string_append_with_format(&accumulated, "%s%s", prefix, element);
-    return accumulated;
 }
